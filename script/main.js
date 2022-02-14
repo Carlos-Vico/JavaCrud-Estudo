3b525c8802be48a8f4def7f30c41c1882b7f3a12
Carlos-Vico <carlos_vieira-costa@estudante.sesisenai.org.br>
2022-02-14 18:37:40 -0300

    correção do validador

--- script/main.js
+++ script/main.js
@@ -8,9 +8,11 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
     
 }
+    // ?? [] - se o banco estiver vazio retorna uma Array vazia
+    const pegaLocalStorage = () => JSON.parse(localStorage.getItem('db_clientes')) ?? [] 
 
-    const pegaLocalStorage = () => JSON.parse(localStorage.getItem('db_clientes')) ?? [] // ?? [] - se o banco estiver vazio retorna uma string vazia
-    const insereNoLocalStorage = (dbCliente) => localStorage.setItem('db_clientes', JSON.stringify(dbCliente))//JSON.stringify converte o objeto para string
+    //JSON.stringify converte o objeto para string
+    const insereNoLocalStorage = (dbCliente) => localStorage.setItem('db_clientes', JSON.stringify(dbCliente))
 
 
 //================ METODOS CRUD ================
@@ -34,8 +36,8 @@ const closeModal = () => {
 
     // CREATE
     const criarCliente = (c) => {
-        // Fazendo parse do que esta no banco para formato Json com JSON.parse
-        // E inserindo numa lista db_clientes[]
+        // Fazendo parse do que esta no banco para o formato Json com JSON.parse
+        // E inserindo na variável db_clientes[]
         const dbCliente = pegaLocalStorage()
         dbCliente.push(c)
         insereNoLocalStorage(dbCliente)
@@ -114,38 +116,3 @@ document.getElementById('fechaModal')
 
     document.getElementById('salvar')
     .addEventListener('click', salvarCliente)
-
-
-
-    // const db_clientes = JSON.parse(localStorage.getItem('db_cliente')) // Cria uma array que aramazena os dados existentes no banco  convertidos para formato JSON
-    // db_clientes.push(cliente)
-    // // JSON.stringify converte o objeto para o formato String
-    // localStorage.setItem("db.cliente", "TesteDeInserção");
-    // // localStorage.setItem("db.cliente", JSON.stringify(db_clientes))
-
-// criarCliente(tempCliente)
-
-// nome: "Godzila",
-// email: "zila@teste.com",
-// celular: "11993331111",
-// cidade: "Osaka"
-
-// nome: "Bart",
-// email: "bart@teste.com",
-// celular: "11443331111",
-// cidade: "Sprinfield"
-
-// nome: "E.T",
-// email: "et@teste.com",
-// celular: "119773331111",
-// cidade: "Lua"
-
-// nome: "Sofia",
-// email: "sofi@teste.com",
-// celular: "116773331111",
-// cidade: "Ivaiporã"
-
-// nome: "Lemmy Kilmister",
-// email: "naldo@teste.com",
-// celular: "55632323233",
-// cidade: "Los Angeles"
